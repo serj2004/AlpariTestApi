f1bcf0b3ca3294f04a6c79af690085a88b2896b7
Sergey <serg_2004@inbox.ru>
2020-07-27 10:01:03 +0700

    Add files via upload

---
+++ AlpariTestApi.py
@@ -0,0 +1,27 @@
+import json
+import requests
+
+
+class TestAlpariApi:
+    URL = "https://alpari.com/api/ru/forex_instruments/currency_converter/rate/?"
+
+    HEADERS = {"User-Agent": "Test", "Referer": "https://alpari.com"}
+
+    def test_func(self, src, dst):
+        res = requests.get(self.URL + 'src=' + src + '&' + 'dst=' + dst, headers=self.HEADERS)
+        body = json.loads(res.content)
+        try:
+            assert res.status_code == 200
+            assert 'application/json' in res.headers['Content-Type']
+            assert body["success"] is True
+            rate = str(body["data"]["rate"])
+            assert rate.isdigit
+        except AssertionError:
+            return False
+        else:
+            return True
+
+
+Tst = TestAlpariApi()
+r = Tst.test_func('EUR', 'USD')
+print(r)
